d6f7f457b07cd6847a71ba4072b46ee42d85306b
Jimmy Le <jimmyle414@gmail.com>
2020-02-27 01:23:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -32,13 +32,3 @@ end
 
 
 
-def input_to_index(user_input)
-  new_user_input = user_input.to_i
-  new_user_input -= 1
-  return new_user_input
-end
-
-def move(board, index, character = "X")
-  board[index] = character
-  return board
-end
