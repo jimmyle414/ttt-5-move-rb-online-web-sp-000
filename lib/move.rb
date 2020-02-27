b63418bce854d6ddb261027391f7598c84fd3bca
Jimmy Le <jimmyle414@gmail.com>
2020-02-27 00:40:22 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(user_input)
+def index = input_to_index(user_input)
   puts "Please enter 1-9:"
   user_input = gets.strip
 end
