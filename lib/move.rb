1046cc8337184d3dd10a34f91b906c993d9efedd
Jimmy Le <jimmyle414@gmail.com>
2020-02-27 00:56:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 #Take players 1-9 input and turn it into a board input 0-8
 
 def index = input_to_index(input)
-  new_user_input = 
+  new_user_input = user_input.to_i - 1
 
   input = index.to_i - 1
 end
